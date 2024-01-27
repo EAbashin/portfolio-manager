7744bb667f5e7de3be5b93796f1f752ae1cd8941
Evgeniy Abashin <aevgen.001@gmail.com>
2024-01-28 00:11:15 +0300

    feat: добавляет React и scss

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-export const someFn = (n: number): string => {
-  for (let index = 0; index < n; index++) {
-    console.log("some function");
-  }
-
-  document.body.innerHTML = `<div>HELLO WORLD</div>`;
-
-  return "number";
-};
