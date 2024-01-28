ab7b701667f71d1c289e30c173fd387f2b002c16
Evgeniy Abashin <aevgen.001@gmail.com>
2024-01-28 19:05:06 +0300

    feat: добавляет react-router-dom и code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Counter } from "./components/Counter";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./index.scss";
+import MainPageLazy from "./pages/MainPage/MainPage.lazy";
+import AboutPageLazy from "./pages/AboutPage/AboutPage.lazy";
+import { Suspense } from "react";
 
 const App = () => {
   return (
-    <div>
-      <Counter />
+    <div className="App">
+      <BrowserRouter>
+        <Link to={"/"}>Main</Link>
+        <Link to={"/about"}>About</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<MainPageLazy />} />
+            <Route path="/about" element={<AboutPageLazy />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 };
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import classes from "./Counter.module.scss";
 
-export const Counter = () => {
+const Counter = () => {
   const [count, setCount] = useState(0);
   const increment = () => {
     setCount(count + 1);
@@ -16,3 +16,5 @@ export const Counter = () => {
     </div>
   );
 };
+
+export default Counter;
